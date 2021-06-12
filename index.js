1572787f9d7487a5e6d4a3c296fe710619560d12
Cory <oneillco117@gmail.com>
2021-06-12 16:16:44 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution here
+function findMatching(drivers){
+    const filtered = drivers.filter(name => name.drivers === "Bobby" && "bobby")
+    return filtered
+}
+
+//let found = names.filter(element => element.drivers === 'Bobby')
+// if(found){
+//     return found.drivers
+//         }
