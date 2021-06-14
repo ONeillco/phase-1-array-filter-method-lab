92dc6443fa6644ec77a21351b6b51eb71ca605b8
Cory <oneillco117@gmail.com>
2021-06-14 10:44:14 -0400

    Done.

--- index.js
+++ index.js
@@ -2,21 +2,34 @@
 function findMatching(driversArray, nameToFind){
     //const filtered = drivers.filter(name => name.drivers === "Bobby" && "bobby")
     const filtered = driversArray.filter(name => nameToFind.toLowerCase() === name.toLowerCase() )
-    // console.log(filtered)
+     //console.log(filtered)
     return filtered
+    
   }
 
 
   function fuzzyMatch(driversArray, lettersToFind){
     const filtered = driversArray.filter(driverNames => {
-      lettersToFind === driverNames;
+    
+     return driverNames.toLowerCase().startsWith(lettersToFind.toLowerCase());
       
       //console.log('driverNames', driverNames)
-      console.log('lettersTofind', lettersToFind)
+      //console.log('lettersTofind', lettersToFind)
       
     })
     return filtered
   }
+
+  function matchName(driversArray, nameTofind){
+    const filtered = driversArray.filter(driverNames => nameTofind === driverNames)
+    return filtered
+      
+  }
+ 
+//console.log('namesToFind', namesToFind)
+ // console.log('nameLocation', nameLocation)
+  
+
   
 
 
